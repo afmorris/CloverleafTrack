d88bab473250a78238c396482cecc2e836a92657
Tony Morris <tony@morriscloud.com>
2025-04-30 14:40:03 -0400

    Initial add

---
+++ CloverleafTrack.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CloverleafTrack.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
