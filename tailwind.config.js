8fa67df44aad7f52f058a06efc705d3e93461bd4
Tony Morris <tony@morriscloud.com>
2025-05-07 14:39:22 -0400

    Template fun!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      './CloverleafTrack.Web/Views/**/*.cshtml',
+      './CloverleafTrack.Web/wwwroot/js/**/*.js'
+  ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            ctf: {
+                nav: '#233625',
+                footer: '#e7f5e7',
+                accent: '#67c379'
+            }
+        },
+        fontFamily: {
+            heading: ['Montserrat', 'sans-serif'],
+            body: ['Inter', 'sans-serif'],
+        },
+    },
   },
+    darkMode: 'class',
   plugins: [],
 }
 
